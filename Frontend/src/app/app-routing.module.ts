eb1aec065a233036252d5453b678b51c7161b763
thanhht99 <hoangtienthanh1999@gmail.com>
2020-10-15 11:54:09 +0700

    edit frontend

--- Frontend/src/app/app-routing.module.ts
+++ Frontend/src/app/app-routing.module.ts
@@ -2,12 +2,16 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { LoginComponent } from './login/login.component';
 import { RegisterComponent } from './register/register.component';
-import {HomeComponent } from './home/home.component';
+import { HomeComponent } from './home/home.component';
+import { TourGuideInfoComponent } from './tour-guide-info/tour-guide-info.component';
+import { CustomerInfoComponent } from './customer-info/customer-info.component';
 
 const routes: Routes = [
   { path: 'register', component: RegisterComponent},
-  { path: 'login',component:LoginComponent},
-  {path: '', component: HomeComponent}
+  { path: 'login',component: LoginComponent},
+  { path: 'customerinfo', component: CustomerInfoComponent},
+  { path: 'tourguideinfo', component: TourGuideInfoComponent},
+  { path: '', component: HomeComponent}
 ];
 
 @NgModule({
