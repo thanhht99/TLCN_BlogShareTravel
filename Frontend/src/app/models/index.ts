6e8c1fc9592016ade100dbd3fee64e14de78066b
thanhht99 <hoangtienthanh1999@gmail.com>
2020-12-07 17:28:41 +0700

    edit model blog

--- Frontend/src/app/models/index.ts
+++ Frontend/src/app/models/index.ts
@@ -2,6 +2,7 @@
 export * from './customer.model';
 export * from './tour.model';
 export * from './trip.model';
+export * from './blog.model';
 export * from './register-trip.model';
 export * from './tour-guide.model';
 export * from './alert';
